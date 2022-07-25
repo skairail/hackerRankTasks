fa87666e80b7ed7004388f751de18374570990b2
Olek <61022995+skairail@users.noreply.github.com>
2022-07-25 14:34:55 +0300

    Update alltasks.js

--- alltasks.js
+++ alltasks.js
@@ -112,8 +112,8 @@ function matchingStrings(strings, queries) {
  // EXTRA LONG FACTORIALS(MEDIUM) https://www.hackerrank.com/challenges/extra-long-factorials/problem
 
  function extraLongFactorials(n) {
-    var bigInt = BigInt(n);
-    var factorial = 1n;
+    let bigInt = BigInt(n);
+    let factorial = 1n;
 
     for (let i = 0n; i < bigInt ; i++) {
         factorial *= bigInt - i;
@@ -148,4 +148,297 @@ function encryption(s) {
      }
         let joindeStr =  globalArr.join(' ')
         return joindeStr
+}
+
+// The Power Sum (Medium https://www.hackerrank.com/challenges/the-power-sum/problem?h_r=profile
+
+function powerSum(X, N, exponentIncrement = 1) {
+    let exponentVal = X - Math.pow(exponentIncrement, N);
+    if (exponentVal < 0){ 
+        return 0;
+    }
+    else if(exponentVal === 0){
+         return 1;
+    } 
+    else return powerSum(exponentVal, N, exponentIncrement + 1) + powerSum(X, N, exponentIncrement + 1);
+}
+
+
+// Gaming Array (MEDIUM) https://www.hackerrank.com/challenges/an-interesting-game-1/submissions/code/280944594
+
+function gamingArray(arr) {
+    let maxInt = arr[0];
+    let counter = 1;
+    if(arr.length === 1){
+        return "BOB";
+    }
+    for(let i = 1; i < arr.length; i++) {
+        if(arr[i] > maxInt){
+            maxInt = arr[i];
+            counter++
+        }
+    }
+
+    if(counter%2 === 0) {
+        return "ANDY";
+    }
+    else {
+        return "BOB"
+    }
+}
+
+class LinkedListNode {
+        constructor(value, next = null)
+        {
+            this.value = value;
+            this.next = next;
+        }
+
+
+        toString () {
+            return `${this.value}`
+        }
+    }
+
+
+// Linked list implementation
+
+class LinkedList 
+{
+    constructor () {
+        this.head = null
+        this.tail = null
+    }
+    append(value)
+    {
+        const newNode = LinkedListNode(value);
+
+        if (!this.head || !this.tail)
+        {
+            this.head = newNode;
+            this.tail = newNode;
+
+            return this
+        }
+
+        this.tail.text = newNode;
+
+        this.tail = newNode;
+
+        return this;
+    }
+    toArray ()
+    {
+        const nodes = [];
+
+        let currentNode = this.head;
+
+        while(currentNode)
+        {
+            nodes.push(currentNode)
+            currentNode = currentNode.next;
+        }
+        return nodes;
+        1
+    }
+}
+
+// Stack implementation 
+
+
+class Node {
+    constructor(value){
+      this.value = value;
+      this.next = null;
+    }
+  }
+  
+  class Stack {
+    constructor(){
+      this.top = null;
+      this.bottom = null;
+      this.length = 0;
+    }
+    peek() {
+      return this.top;
+    }
+    push(value){
+      const newNode = new Node(value);
+      if (this.length === 0) {
+        this.top = newNode;
+        this.bottom = newNode;
+      } else {
+        const holdingPointer = this.top;
+        this.top = newNode;
+        this.top.next = holdingPointer;
+      }
+      this.length++;
+      return this;
+    }
+    pop() {
+        if (!this.top) {
+            return null;
+        }
+        if (this.top === this.bottom) {
+            this.botom = null;
+        }
+        // const holdingPointer = this.top;
+        this.top = this.top.next;
+        this.length--;
+        return this;
+    }
+}   
+
+
+// Stack implementation with array 
+
+class Node {
+    constructor(value){
+      this.value = value;
+      this.next = null;
+    }
+  }
+  
+  class Stack {
+    constructor() {
+      this.array = [];
+    }
+    peek() {
+      return this.array[this.array.length-1];
+    }
+    push(value) {
+      this.array.push(value)
+      return this;
+    }
+    pop() {
+        this.array.pop()
+        return this;
+    }
+}   
+
+// Binary tree implementation 
+
+class Node {
+    constructor(value)
+    {
+        this.value = value;
+        this.left = null;
+        this.right = null;
+    }
+}
+
+class BinaryTree {
+    constructor()
+    {
+        this.root = null;
+    }
+        add(value){
+
+    }     
+
+    preOrder (node,callback)
+    {
+        if(!node)
+        {
+            return
+        }
+        if (callback)
+        {
+            callback(node);
+        }
+
+        this.preOrder(node.left, callback)
+        this.preOrder(node.right, callback)
+    }
+
+
+
+    traverseDFS(callback,method) {
+        if(method = 'preOrder')
+        {
+            return this.preOrder(this.root,callback)
+        }
+        if (method = 'inOrder')
+        {
+            return this.inOrder(this.root,callback)
+        }
+
+        return this.postOrder(this.root,callback)
+    } 
+
+}
+
+
+// Selection sort 
+
+
+function selectionSort(array) {
+  const length = array.length;
+  for(let i = 0; i < length; i++){
+    let min = i;
+    let temp = array[i];
+    for(let j = i+1; j < length; j++){
+      if (array[j] < array[min]){
+        min = j;
+      }
+    }
+    array[i] = array[min];
+    array[min] = temp;
+  }
+  return array;
+}
+
+// Merge sort \
+
+
+function mergeSort (array) {
+  if (array.length === 1) {
+    return array
+  }
+  const length = array.length;
+  const middle = Math.floor(length / 2)
+  const left = array.slice(0, middle) 
+  const right = array.slice(middle)
+
+  
+  return merge(
+    mergeSort(left),
+    mergeSort(right)
+  )
+}
+
+function merge(left, right){
+  const result = [];
+  let leftIndex = 0;
+  let rightIndex = 0;
+  while(leftIndex < left.length && 
+        rightIndex < right.length){
+     if(left[leftIndex] < right[rightIndex]){
+       result.push(left[leftIndex]);
+       leftIndex++;
+     } else{
+       result.push(right[rightIndex]);
+       rightIndex++
+    }
+  }  
+  return result.concat(left.slice(leftIndex)).concat(right.slice(rightIndex));
+}
+
+// Insertion sort 
+
+function insertionSort(array) {
+  const length = array.length;
+	for (let i = 0; i < length; i++) {
+		if (array[i] < array[0]) {
+      array.unshift(array.splice(i,1)[0]);
+    } else {
+      if (array[i] < array[i-1]) {
+        for (var j = 1; j < i; j++) {
+          if (array[i] >= array[j-1] && array[i] < array[j]) {
+            array.splice(j,0,array.splice(i,1)[0]);
+          }
+        }
+      }
+    }
+	}
 }
